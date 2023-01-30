a118cd0790a1a2a541c9dce3b0f323ae5a05c73a
pinkros <arvid.leimar@iths.se>
2023-01-30 15:00:30 +0100

    here we commit

--- script.js
+++ script.js
@@ -7,6 +7,56 @@ console.log(`letVar är ${letVar}`);
 console.log(`varVar är ${varVar}`);
 console.log(`Denna sida lovar att inte mine:a bitcoin med din dator`);
 
-if (condition) {
-} else {
+// 1. Skapa en array med bilmärkesnaman:
+//     ['bmw', 'mercedes', 'volvo', 'opel']
+// 2. lägg till märket 'saab' först i listan
+// 3. lägg till 'tesla' sist
+// 4. leta upp 'saab' utan att plocka bort
+//      och printa ut det
+// 5. skapa en till array med fler märken
+//     ['renault', 'skoda']
+// 6. lägg till den nya arrayen från index 2 i den första
+
+const carManufacurer = ["bmw", "mercedes", "volvo", "opel"];
+
+console.log("första listan:");
+
+for (const car of carManufacurer) {
+	console.log(car);
+}
+
+carManufacurer.unshift("saab");
+
+console.log("andra listan:");
+
+for (const car of carManufacurer) {
+	console.log(car);
+}
+
+carManufacurer.push("tesla");
+
+console.log("tredje listan:");
+
+for (const car of carManufacurer) {
+	console.log(car);
+}
+
+let saab = carManufacurer.indexOf("volvo");
+
+console.log(`hittat ${carManufacurer[saab]}`);
+
+console.log("verifierar att volvo inte försvunnit:");
+
+for (const car of carManufacurer) {
+	console.log(car);
+}
+
+const moreCarManuf = ["renault", "skoda"];
+
+carManufacurer.splice(2, 0, moreCarManuf[1], moreCarManuf[0]);
+
+console.log("splicey splicey:");
+
+for (const car of carManufacurer) {
+	console.log(car);
 }
