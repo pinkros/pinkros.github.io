205687d68f7235a611f3cf3ec8a77aaf25e91f29
pinkros <arvid.leimar@iths.se>
2023-01-30 11:58:44 +0100

    do some stuff

---
+++ script.js
@@ -0,0 +1,12 @@
+const constVar = 1;
+let letVar = 2;
+var varVar = 3;
+
+console.log(`constVar är ${constVar}`);
+console.log(`letVar är ${letVar}`);
+console.log(`varVar är ${varVar}`);
+console.log(`Denna sida lovar att inte mine:a bitcoin med din dator`);
+
+if (condition) {
+} else {
+}
